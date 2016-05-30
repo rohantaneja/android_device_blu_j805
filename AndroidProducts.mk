8d3aa0fd8b0bef9d5c6af74bdb02be4dc1cf0141
Rohan Taneja <rohantaneja63@gmail.com>
2016-05-30 14:24:26 +0530

    added more stuff

--- AndroidProducts.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_AQ5001.mk
--- cm.mk
+++ cm.mk
@@ -1,19 +1,19 @@
 # Release name
-PRODUCT_RELEASE_NAME := AQ5001
+PRODUCT_RELEASE_NAME := Q2500
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/micromax/AQ5001/device_AQ5001.mk)
+$(call inherit-product, device/blu/j805/device.mk)
 
 # Correct bootanimation size for the screen
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_AQ5001
-PRODUCT_DEVICE :=AQ5001
-PRODUCT_BRAND := micromax
-PRODUCT_MANUFACTURER := micromax
-PRODUCT_MODEL := AQ5001 Canvas Power
+PRODUCT_NAME := cm_j805
+PRODUCT_DEVICE := XOLO Q2500
+PRODUCT_BRAND := blu
+PRODUCT_MANUFACTURER := blu
+PRODUCT_MODEL := XOLO Q2500
